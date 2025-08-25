73ed1a0276c7e27c9afed0af3adb63afe50f83ba
Ben-TECHPlayer <158784500+Ben-TECHPlayer@users.noreply.github.com>
2025-08-25 11:08:47 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,109 @@
-function toggleMenu() {
-    const dropdown = document.getElementById("dropdown");
-    dropdown.classList.toggle("open");
-    dropdown.style.width = menuButton.offsetWidth + 'px';
-
-    document.addEventListener("click", function(event) {
-        const menu = document.querySelector(".menu");
-        const dropdown = document.getElementById("dropdown");
-
-        // Si on clique ni sur le bouton, ni sur le menu
-        if (!menu.contains(event.target) && !dropdown.contains(event.target)) {
-            dropdown.classList.remove("open");
-        }
-    });
-}
-
 function showSection(sectionId) {
     const hard = document.getElementById('hardskills');
     const soft = document.getElementById('softskills');
+    const hardBtn = document.getElementById('btn-hardskills');
+    const softBtn = document.getElementById('btn-softskills');
+
     const all = document.getElementById('every');
     const sti2d = document.getElementById('sti2d');
     const but = document.getElementById('but');
     const home = document.getElementById('home');
+    const allBtn = document.getElementById('btn-every');
+    const sti2dBtn = document.getElementById('btn-sti2d');
+    const butBtn = document.getElementById('btn-but');
+    const homeBtn = document.getElementById('btn-home');
+
     const work = document.getElementById('works');
     const salon = document.getElementById('salons');
     const intern = document.getElementById('internship');
     const jobs = document.getElementById('full-time');
+    const workBtn = document.getElementById('btn-works');
+    const salonBtn = document.getElementById('btn-salons');
+    const internBtn = document.getElementById('btn-internship');
+    const jobsBtn = document.getElementById('btn-full-time');
 
 
     if (sectionId === 'hardskills') {
         hard.style.display = 'grid';
         soft.style.display = 'none';
+        hardBtn.style.color = 'greenyellow';
+        softBtn.style.color = '';
     } else if (sectionId === 'softskills') {
         hard.style.display = 'none';
         soft.style.display = 'grid';
+        hardBtn.style.color = '';
+        softBtn.style.color = 'greenyellow';
     } else if (sectionId === 'every') {
         all.style.display = 'grid';
         sti2d.style.display = 'none';
         but.style.display = 'none';
         home.style.display = 'none';
+        allBtn.style.color = 'greenyellow';
+        sti2dBtn.style.color = '';
+        butBtn.style.color = '';
+        homeBtn.style.color = '';
     } else if (sectionId === 'sti2d') {
         all.style.display = 'none';
         sti2d.style.display = 'grid';
         but.style.display = 'none';
         home.style.display = 'none';
+        allBtn.style.color = '';
+        sti2dBtn.style.color = 'greenyellow';
+        butBtn.style.color = '';
+        homeBtn.style.color = '';
     } else if (sectionId === 'but') {
         all.style.display = 'none';
         sti2d.style.display = 'none';
         but.style.display = 'grid';
         home.style.display = 'none';
+        allBtn.style.color = '';
+        sti2dBtn.style.color = '';
+        butBtn.style.color = 'greenyellow';
+        homeBtn.style.color = '';
     } else if (sectionId === 'home') {
         all.style.display = 'none';
         sti2d.style.display = 'none';
         but.style.display = 'none';
         home.style.display = 'grid';
+        allBtn.style.color = '';
+        sti2dBtn.style.color = '';
+        butBtn.style.color = '';
+        homeBtn.style.color = 'greenyellow';
     } else if (sectionId === 'works') {
         work.style.display = 'grid';
         salon.style.display = 'none';
         intern.style.display = 'none';
         jobs.style.display = 'none';
+        workBtn.style.color = 'greenyellow';
+        salonBtn.style.color = '';
+        internBtn.style.color = '';
+        jobsBtn.style.color = '';
     } else if (sectionId === 'salons') {
         work.style.display = 'none';
         salon.style.display = 'grid';
         intern.style.display = 'none';
         jobs.style.display = 'none';
+        workBtn.style.color = '';
+        salonBtn.style.color = 'greenyellow';
+        internBtn.style.color = '';
+        jobsBtn.style.color = '';
     } else if (sectionId === 'internship') {
         work.style.display = 'none';
         salon.style.display = 'none';
         intern.style.display = 'grid';
         jobs.style.display = 'none';
+        workBtn.style.color = '';
+        salonBtn.style.color = '';
+        internBtn.style.color = 'greenyellow';
+        jobsBtn.style.color = '';
     } else if (sectionId === 'full-time') {
         work.style.display = 'none';
         salon.style.display = 'none';
         intern.style.display = 'none';
         jobs.style.display = 'grid';
+        workBtn.style.color = '';
+        salonBtn.style.color = '';
+        internBtn.style.color = '';
+        jobsBtn.style.color = 'greenyellow';
     }
 }
