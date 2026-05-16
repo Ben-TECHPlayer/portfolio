5cebc850dd45de547ff6c39b68740990f99dfc62
Ben-Ali ABOUDOU <ben-ali.aboudou@universite-paris-saclay.fr>
2026-05-16 03:08:34 +0200

    Mon portfolio version 2.0 est fait avec un code plus propre

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,51 @@
-function showSection(sectionId) {
+function showSection(sectionId){
+    // Déclarer les variables pour le système de filtres de compétences et qualités
     const hard = document.getElementById('hardskills');
     const soft = document.getElementById('softskills');
-    const hardBtn = document.getElementById('btn-hardskills');
-    const softBtn = document.getElementById('btn-softskills');
+    const hardButton = document.getElementById('bouton-hardskills');
+    const softButton = document.getElementById('bouton-softskills');
 
+    // Déclarer les variables pour le système de filtres de projets informatiques
     const webdev = document.getElementById('web-dev');
     const webdesign = document.getElementById('web-design');
-    const mobiledev = document.getElementById('mobile');
-    const webdevBtn = document.getElementById('btn-webdev');
-    const webdesignBtn = document.getElementById('btn-webdesign');
-    const mobileBtn = document.getElementById('btn-mobile');
+    const appdev = document.getElementById('app-dev');
+    const webdevButton = document.getElementById('bouton-webdev');
+    const webdesignButton = document.getElementById('bouton-webdesign');
+    const appdevButton = document.getElementById('bouton-appdev');
 
-    // const work = document.getElementById('works');
-    // // const salon = document.getElementById('salons');
-    // const intern = document.getElementById('internship');
-    // const jobs = document.getElementById('full-time');
-    // const workBtn = document.getElementById('btn-works');
-    // // const salonBtn = document.getElementById('btn-salons');
-    // const internBtn = document.getElementById('btn-internship');
-    // const jobsBtn = document.getElementById('btn-full-time');
-
-
-    if (sectionId === 'hardskills') {
+    // Utiliser ces variables pour le système de filtres de compétences et qualités
+    if(sectionId === 'hardskills'){
         hard.style.display = 'flex';
         soft.style.display = 'none';
-        hardBtn.style.color = '#007bff';
-        softBtn.style.color = '';
-    } else if (sectionId === 'softskills') {
+        hardButton.style.color = '#007bff';
+        softButton.style.color = '';
+    } else if(sectionId === 'softskills'){
         hard.style.display = 'none';
         soft.style.display = 'flex';
-        hardBtn.style.color = '';
-        softBtn.style.color = '#007bff';
-    } else if (sectionId === 'web-dev') {
+        hardButton.style.color = '';
+        softButton.style.color = '#007bff';
+    }
+    // Utiliser les variables pour le système de filtres de projets informatiques
+    else if(sectionId === 'web-dev'){
         webdev.style.display = 'flex';
         webdesign.style.display = 'none';
-        mobiledev.style.display = 'none';
-        webdevBtn.style.color = '#007bff';
-        webdesignBtn.style.color = '';
-        mobileBtn.style.color = '';
-    } else if (sectionId === 'web-design') {
+        appdev.style.display = 'none';
+        webdevButton.style.color = '#007bff';
+        webdesignButton.style.color = '';
+        appdevButton.style.color = '';
+    } else if(sectionId === 'web-design'){
         webdev.style.display = 'none';
         webdesign.style.display = 'flex';
-        mobiledev.style.display = 'none';
-        webdevBtn.style.color = '';
-        webdesignBtn.style.color = '#007bff';
-        mobileBtn.style.color = '';
-    } else if (sectionId === 'mobile') {
+        appdev.style.display = 'none';
+        webdevButton.style.color = '';
+        webdesignButton.style.color = '#007bff';
+        appdevButton.style.color = '';
+    } else if(sectionId === 'app-dev'){
         webdev.style.display = 'none';
         webdesign.style.display = 'none';
-        mobiledev.style.display = 'flex';
-        webdevBtn.style.color = '';
-        webdesignBtn.style.color = '';
-        mobileBtn.style.color = '#007bff';
-    } 
+        appdev.style.display = 'flex';
+        webdevButton.style.color = '';
+        webdesignButton.style.color = '';
+        appdevButton.style.color = '#007bff';
+    }
 }
